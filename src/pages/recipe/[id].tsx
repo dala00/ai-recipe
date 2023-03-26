7b742e8d561cf5b0a3ba1ff2681e2e919c06ddb8
Shoichi Namba <alphabrend@gmail.com>
2023-03-26 16:44:47 +0900

    シェアURLの不具合修正

--- src/pages/recipe/[id].tsx
+++ src/pages/recipe/[id].tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import {
   Box,
   Button,
@@ -6,97 +5,95 @@ import {
   Container,
   Heading,
   Image,
-  Spinner,
   Text,
 } from '@chakra-ui/react'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
 import { Recipe } from '@prisma/client'
-import { GetRecipeResponseData } from '@/types/response/recipe'
 import Link from 'next/link'
+import { GetServerSideProps } from 'next'
+import { prisma } from '@/lib/db'
 
 type Query = {
   id: string
 }
 
-export default function BattlePage() {
-  const router = useRouter()
-  const { id } = router.query as Query
-  const [recipe, setRecipe] = useState<Recipe>()
+type Props = {
+  recipe: Recipe
+}
+
+export default function BattlePage({ recipe }: Props) {
+  const [hostname, setHostname] = useState('')
+  const id = recipe.id
   const shareUrl = useMemo(() => {
-    if (typeof window === 'undefined') {
-      return ''
-    }
     const text = recipe?.ingredients.replace(/\n/g, ',') + 'で作れるレシピは…？'
-    const url = `https://${location.hostname}/recipe/${id}`
+    const url = `https://${hostname}/recipe/${id}`
     return `https://twitter.com/intent/tweet?url=${encodeURIComponent(
       url
     )}&text=${encodeURIComponent(text)}&hashtags=${encodeURIComponent(
       'AIレシピジェネレータ'
     )}`
-  }, [id, typeof window])
-
-  const initialize = useCallback(async () => {
-    const response = await axios
-      .get<GetRecipeResponseData>(`/api/recipe/${id}`)
-      .catch((error) => {
-        console.error(error)
-        return null
-      })
-    if (!response) {
-      router.push('/')
-      return
-    }
-
-    setRecipe(response.data.recipe)
-  }, [id])
+  }, [id, hostname])
 
   useEffect(() => {
-    if (!id) {
-      return
+    if (typeof window !== undefined) {
+      setHostname(location.hostname)
     }
-    initialize()
-  }, [id])
+  }, [typeof window])
 
   return (
     <Container>
-      {recipe ? (
-        <>
-          <Heading size="md">指定した食材</Heading>
-          <Text mt={4} whiteSpace="pre-wrap" wordBreak="break-word">
-            {recipe.ingredients}
-          </Text>
-          <Heading size="md" mt={8}>
-            レシピ
-          </Heading>
-          <Text mt={4} whiteSpace="pre-wrap" wordBreak="break-word">
-            {recipe.recipe}
-          </Text>
-          <Text mt={4} fontSize={12} color="gray.500">
-            ※ AIが生成しているため正しくない情報の場合があります
-          </Text>
-          <Box mt={8} textAlign="center">
-            <a href={shareUrl} target="_blank" rel="nofollow noopener">
-              <Button type="button" colorScheme="twitter">
-                このレシピをTwitterでシェア
-              </Button>
-            </a>
-          </Box>
-          <Center mt={8}>
-            <Image src="/images/takuhai_yasai_box.png" alt="" maxHeight={40} />
-          </Center>
-          <Box mt={4} textAlign="center">
-            <Link href="/">
-              <Box textDecoration="underline">他の食材も試す</Box>
-            </Link>
-          </Box>
-        </>
-      ) : (
-        <Center>
-          <Spinner />
-        </Center>
-      )}
+      <Heading size="md">指定した食材</Heading>
+      <Text mt={4} whiteSpace="pre-wrap" wordBreak="break-word">
+        {recipe.ingredients}
+      </Text>
+      <Heading size="md" mt={8}>
+        レシピ
+      </Heading>
+      <Text mt={4} whiteSpace="pre-wrap" wordBreak="break-word">
+        {recipe.recipe}
+      </Text>
+      <Text mt={4} fontSize={12} color="gray.500">
+        ※ AIが生成しているため正しくない情報の場合があります
+      </Text>
+      <Box mt={8} textAlign="center">
+        <a href={shareUrl} target="_blank" rel="nofollow noopener">
+          <Button type="button" colorScheme="twitter">
+            このレシピをTwitterでシェア
+          </Button>
+        </a>
+      </Box>
+      <Center mt={8}>
+        <Image src="/images/takuhai_yasai_box.png" alt="" maxHeight={40} />
+      </Center>
+      <Box mt={4} textAlign="center">
+        <Link href="/">
+          <Box textDecoration="underline">他の食材も試す</Box>
+        </Link>
+      </Box>
     </Container>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const { id } = context.query as Query
+
+  const recipe = await prisma.recipe.findFirst({
+    where: {
+      id,
+    },
+  })
+
+  if (!recipe) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      recipe: JSON.parse(JSON.stringify(recipe)),
+    },
+  }
 }
--- src/types/response/recipe.ts
+++ src/types/response/recipe.ts
@@ -3,7 +3,3 @@ import { Recipe } from '@prisma/client'
 export type GenerateRecipeResponseData = {
   recipe: Recipe
 }
-
-export type GetRecipeResponseData = {
-  recipe: Recipe
-}
